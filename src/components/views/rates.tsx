a81c0876d7778fab6802dd9a77b393edfe9ec055
Евгений Сомов <e.somov@teamforce.ru>
2025-08-08 16:53:14 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/views/rates.tsx
+++ src/components/views/rates.tsx
@@ -33,7 +33,7 @@ export function RatesView({ data }: { data: MessageData[] }) {
         <KpiCard title="Средняя ставка" value={`${Math.round(averageRate)}`} icon={RussianRuble} description="руб/ч" />
       </div>
       
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 md:gap-8">
+      <div className="grid gap-4 md:gap-8 lg:grid-cols-2">
          <Card>
             <CardHeader>
             <CardTitle>Средние ставки по ролям (ТОП-10)</CardTitle>
