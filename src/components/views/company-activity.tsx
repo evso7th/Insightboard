0df44202e2aec5dda5ef48ce2dd247134de2018f
Евгений Сомов <e.somov@teamforce.ru>
2025-08-08 16:10:51 +0000

    Ты точно издеваешся. сколько можно решать элементарную школьную задачу с

--- src/components/views/company-activity.tsx
+++ src/components/views/company-activity.tsx
@@ -153,7 +153,7 @@ export function CompanyActivityView({ data }: { data: MessageData[] }) {
 
   return (
     <div className="flex flex-col gap-4 md:gap-8">
-       <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
          <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                 <CardTitle className="text-sm font-medium">Топ по предложениям</CardTitle>
