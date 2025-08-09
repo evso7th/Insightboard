9f0c6b73786a4bcc9df5c25853fb3e1586dd3125
Евгений Сомов <e.somov@teamforce.ru>
2025-08-09 12:27:36 +0000

    проверь next.config.ts,чтобы там ничего не было про server

---
+++ src/next.config.ts
@@ -0,0 +1,32 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'export',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    // allowedDevOrigins is now a top-level property
+  },
+ allowedDevOrigins: [
+    'https://*.cloudworkstations.dev',
+    'https://*.firebase.studio',
+  ],
+};
+
+export default nextConfig;
